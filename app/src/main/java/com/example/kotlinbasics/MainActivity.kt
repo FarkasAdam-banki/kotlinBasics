380b25df75303a70611544fec06c117403d9b44d
FarkasAdam-banki <77265777+FarkasAdam-banki@users.noreply.github.com>
2024-12-02 12:06:45 +0100

    első commit:project létrehozás

---
+++ app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.kotlinbasics
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    private lateinit var openGreetingButton:Button;
+    private lateinit var openCalculatorButton: Button;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+       // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+          //  insets
+        //}
+
+        openGreetingButton = findViewById(R.id.openGreetingButton)
+        openCalculatorButton = findViewById(R.id.openCalculatorButton)
+
+        openGreetingButton.setOnClickListener(){
+            val intent = Intent(this,GreetingsActivity::class.java)
+            startActivity(intent)
+        }
+        openCalculatorButton.setOnClickListener(){
+            val intent = Intent(this,CalculatorActivity::class.java)
+            startActivity(intent)
+        }
+   }
+
+}
---
+++ app/src/main/java/com/example/kotlinbasics/model/WeatherResponse.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinbasics.model
+
+
+data class WeatherResponse(
+    val main: Main,
+)
+
+data class Main(
+    val temp: Float,
+)
+
+
